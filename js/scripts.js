cda999c974e77e6a48c30e65f06e290d00d0ee41
ahhaahhaah <hh20001111@gmail.com>
2025-03-27 21:26:08 +0100

    final

---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+/*!
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+    
+    function reveal() {
+        var reveals = document.querySelectorAll(".page-section");
+        
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 150;
+            
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("fadeInUp");
+            }
+        }
+    }
+    
+    window.addEventListener("scroll", reveal);
+    reveal(); 
+    $(".portfolio-item").hover(
+        function() {
+            $(this).addClass("active");
+        },
+        function() {
+            $(this).removeClass("active");
+        }
+    );
+    
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    
+    $(".btn-xl").hover(
+        function() {
+            var randomColor = getRandomColor();
+            $(this).css("box-shadow", `0 5px 15px ${randomColor}80`);
+        },
+        function() {
+            $(this).css("box-shadow", "none");
+        }
+    );
+
+    $(".team-member img").click(function() {
+        $(this).toggleClass("clicked");
+    });
+    
+})(jQuery);
